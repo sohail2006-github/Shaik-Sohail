7893757f4d05caced4ccf46c0e858f2b3d24c2f8
sohail2006-github <sohail20060501@gmail.com>
2024-09-17 09:12:09 +0530

    Add files via upload

---
+++ 6.RUN IM NUM OPER.py
@@ -0,0 +1,16 @@
+#ADDTION SUBTERACTION MULTPLICATION AND DIVISON
+a=int(input("a= "))
+b=int(input("b= "))
+c=a+b
+print(c)
+c=a-b
+print(c)
+c=a*b
+print(c)
+c=a**b
+print(c)
+c=a/b
+print(c)
+c=a//b
+print(c)
+
