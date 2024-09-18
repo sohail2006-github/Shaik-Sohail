3a60efde5c7884aeba68ac2254faad4f35215f03
sohail2006-github <sohail20060501@gmail.com>
2024-09-18 14:25:20 +0530

    Add files via upload

---
+++ 7.STUDENT REPORT.py
@@ -0,0 +1,13 @@
+#student report
+name=input("name : ")
+sno=int(input("sno;"))
+tel=int(input("tel : "))
+hin=int(input("hin : "))
+eng=int(input("eng : "))
+tot=tel+hin+eng
+avg=tot/3
+print(name)
+print(sno)
+print("marks in tel ,hin , eng=",tel,hin,eng)
+print(tot)
+print(avg)
